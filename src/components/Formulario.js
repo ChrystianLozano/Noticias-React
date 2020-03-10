f4cde36a95bc5b144450ae8efd4b517a414e43a6
Chrystian Lozano <chrystianlozano@icloud.com>
2020-03-10 14:09:12 -0600

    Mostrando las categorias en select

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import styles from './Formulario.module.css'
+import useSelect from '../Hooks/useSelect'
 const Formulario = () => {
+
+
+    const OPCIONES = [
+        {value: 'technology', label: 'Tecnología'},
+        {value: 'general', label: 'General'},
+        {value: 'business', label: 'Negocios'},
+        {value: 'entertainment', label: 'Entretenimiento'},
+        {value: 'health', label: 'Salud'},
+        {value: 'science', label: 'Ciencia'},
+        {value: 'sports', label: 'Deportes'},
+    ]
+
+
+    //utilizar el costume Hook
+    const[categoria, SelectNoticias] = useSelect('technology', OPCIONES)
+
+
     return ( 
         <div className={`${styles.buscador} row`}>
             <div className="col s12 m8 offset-m2">
                 <form>
                     <h2 className={styles.heading}>Encuentra Noticias por Categorias</h2>
+                    
+                    <SelectNoticias />
+
                     <div className="input-field col s12">
                         <input 
                             type="submit"
