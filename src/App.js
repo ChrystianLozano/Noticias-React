190447e675768024bd26c7ede4773ceb741f7898
Chrystian Lozano <chrystianlozano@icloud.com>
2020-03-11 10:05:04 -0600

    Consultando la API y leyendo los resultados

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Fragment, useState} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
 import Header from './components/Header'
 import Formulario from './components/Formulario'
 
@@ -7,6 +7,22 @@ function App() {
 
   //definir la categoria y noticias
   const [categoria, guardarCategoria] = useState('')
+  const [noticias, guardarNoticias] = useState([])
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `http://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=fef979e63dfa4b108fb49af9549638dd`;
+
+      const respuesta = await fetch(url)
+      const noticias = await respuesta.json()
+
+      guardarNoticias(noticias.articles)
+    }
+    
+    consultarAPI();
+
+  }, [categoria])
+
 
   return (
     <Fragment>
